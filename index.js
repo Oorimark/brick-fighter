92d614f0a30b33894f66f402a31002048356d87c
Oori Mark <oorimark@gmail.com>
2024-04-10 21:06:30 +0100

    Fixed deploy issue of not rendering index.html by removing the app.use(cors)

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import express from "express";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 import path from "path";
-import cors from "cors";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const port = 7021;
-
-app.use(cors);
+const port = 5000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,4 +15,6 @@ app.get("/", (__, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(port, "0.0.0.0", () => {});
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
